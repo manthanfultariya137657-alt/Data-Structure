5add221985addc6b26ab2d13ff9aaea79666929c
manthanfultariya137657-alt <manthan.fultariya137657@marwadiuniversity.ac.in>
2026-07-17 12:09:53 +0530

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, pos, element, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the position to insert (1 to %d): ", n + 1);
+    scanf("%d", &pos);
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &element);
+
+    if(pos < 1 || pos > n + 1) {
+        printf("Invalid position!\n");
+        return 0;
+    }
+
+    for(i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = element;
+    n++;
+
+    printf("Array after insertion:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
